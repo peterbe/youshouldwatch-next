9c2eca62f4118f83a30ad036ecb6f35200b3c13a
Peter Bengtsson <mail@peterbe.com>
2023-01-11 20:08:11 -0500

    favicon

--- src/app/default-tags.tsx
+++ src/app/default-tags.tsx
@@ -4,6 +4,23 @@ export default function DefaultTags() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="You should totally watch..." />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
     </>
   );
 }
