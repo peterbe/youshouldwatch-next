3c6d1edbac5a3e145db012815f50d8afc6603d06
Peter Bengtsson <mail@peterbe.com>
2023-01-12 02:00:18 +0100

    OpenGraph SEO stuff (#3)

---
+++ src/app/default-tags.tsx
@@ -0,0 +1,9 @@
+export default function DefaultTags() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="You should totally watch..." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
