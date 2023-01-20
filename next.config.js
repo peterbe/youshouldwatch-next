8f0a6bdc437c343fbf3415369597d6d40c5e7825
Peter Bengtsson <mail@peterbe.com>
2023-01-19 19:51:54 -0500

    rewrites

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    return [{
+        source: "/__auth/:path*",
+        destination: "https://youshouldwatch-77a46.firebaseapp.com/__auth/:path*",
+
+    }]
+  }
 }
 
 module.exports = nextConfig
